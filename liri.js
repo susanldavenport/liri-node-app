e42dca3390f75ade09e830055cfadf136beb6c3f
SusanLDavenport <susanldavenport@gmail.com>
2018-04-17 17:49:09 -0400

    push it real good

--- liri.js
+++ liri.js
@@ -1,6 +1,25 @@
+
 require("dotenv").config();
 
 var keys = require('./keys.js');
 
-// var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+`my-tweets`
+    `spotify-this-song`
+    `movie-this`
+    `do-what-it-says`
