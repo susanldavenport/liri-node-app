b89972495e4a6411dfc6c8e0db23bb64f698bc73
SusanLDavenport <susanldavenport@gmail.com>
2018-04-18 18:46:17 -0400

    updated commands in liri.js

--- liri.js
+++ liri.js
@@ -1,12 +1,53 @@
-
+// === NPM === //
 require("dotenv").config();
+const keys = require('./keys.js');
+const Twitter = require('twitter');
+const Spotify = require('node-spotify-api');
+const request = require('request');
+// var fs = require('fs');
 
-var keys = require('./keys.js');
+var liriComm = process.argv[2];
+var userInput = process.argv;
+var songTitle;
+var movieTitle;
 
+//=== Keys === //
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
+runLiri();
 
+function runLiri(){
+    switch(liriComm) {
+        case 'my-tweets': 
+        twitterGrab();
+        break;
+
+        case 'spotify-this-song':
+       if (typeof userInput === 0 'undefined') {
+           songName = 'the sign ace of base';
+           spotifyGrab();
+       } else {
+           songName = userInput.join(' ');
+           spotifyGrab();
+           break;
+       }
+
+       case 'movie-this':
+       if (typeof userInput === 0 'undefined') {
+        movieName = 'Mr. Nobody';
+        omdbGrab();
+    } else {
+        movieName = userInput.join(' ');
+        omdbGrab();
+        break;
+    }
+
+        case 'do-what-it-says':
+        doit();
+        break;
+    }
+};
 
 
 
