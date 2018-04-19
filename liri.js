a3b003b089bce850724fee97e7c03367bb0bbd81
SusanLDavenport <susanldavenport@gmail.com>
2018-04-18 22:42:43 -0400

    function addition

--- liri.js
+++ liri.js
@@ -19,34 +19,94 @@ runLiri();
 
 function runLiri(){
     switch(liriComm) {
+
         case 'my-tweets': 
-        twitterGrab();
-        break;
+            twitterGrab();
+            break;
 
         case 'spotify-this-song':
-       if (typeof userInput === 0 'undefined') {
-           songName = 'the sign ace of base';
-           spotifyGrab();
-       } else {
-           songName = userInput.join(' ');
-           spotifyGrab();
-           break;
-       }
+            if (typeof userInput[0] === 'undefined') {
+                songName = 'the sign ace of base';
+                spotifyGrab();
+            } else {
+                songName = userInput.join(' ');
+                spotifyGrab();
+                break;
+            }
 
        case 'movie-this':
-       if (typeof userInput === 0 'undefined') {
-        movieName = 'Mr. Nobody';
-        omdbGrab();
-    } else {
-        movieName = userInput.join(' ');
-        omdbGrab();
-        break;
-    }
+            if (typeof userInput[0] === 'undefined') {
+                movieName = 'Mr. Nobody';
+                omdbGrab();
+            } else {
+                movieName = userInput.join(' ');
+                omdbGrab();
+                break;
+            }
 
         case 'do-what-it-says':
-        doit();
-        break;
+            doit();
+            break;
     }
+};
+
+function twitterGrab() {
+    client.get('https://api.twitter.com/1.1/favorites/list.json', 
+    function(err, tweets, response) {
+        if (err) throw err;
+        tweets.forEach(tweet => {
+            console.log('\n ================================');
+            console.log(`Date: ${tweet.created_at}`);
+            console.log(`\n${tweet.text}`);
+            console.log('\n ================================');
+        })
+    })
+};
+
+
+function spotifyGrab() {
+    spotify.search({type: "track", query: "songName"}, 
+    function(err, data) {
+        if(err) throw err;
+            console.log('\n=====================================');
+            console.log(`Artist: ${data.tracks.items[0].artists[0].name}`);
+            console.log(`Track: ${data.tracks.items[0].name}`); 
+            console.log(`Album: ${data.tracks.items[0].album.name}`);
+            console.log(`Link: ${data.tracks.items[0].album.external_urls.spotify}`);
+            console.log('=====================================');
+        });
+    };
+
+
+function omdbGrab() {
+    const omdbURL = 'http://www.omdbapi.com/?apikey=trilogy&t=${movieName}';
+    request(omdbURL, function(err, response, body){
+        if (!err && response.statusCode === 200) {
+            const data = JSON.parse(body);
+            console.log('\n=====================================');
+            console.log(`Title: ${data.Title}`);
+            console.log(`Year: ${data.Year}`);
+            console.log(`IMDB Rating: ${data.imdbRating}`);
+            console.log(`Rotten Tomatoes Rating: ${data.Ratings[1].Value}`);
+            console.log(`Country: ${data.Country}`);
+            console.log(`Language: ${data.Language}`);
+            console.log(`Actors: ${data.Actors}`);
+            console.log(`Plot: ${data.Plot}`);
+            console.log('=====================================');
+        };
+    });
+};
+
+
+function doIt() {
+    fs.readFile('random.txt', 'utf-8', (err, data) => {
+        if (err) {return console.log(err)}
+        const readFileArr = data.split(',');
+        liriComm = readFileArr[0];
+        userInput = readFileArr[1].split(' ');
+        conosle.log('===============');
+    liriComm();
+    });
 };
 
 
@@ -54,13 +114,3 @@ function runLiri(){
 
 
 
-
-
-
-
-
-
-`my-tweets`
-    `spotify-this-song`
-    `movie-this`
-    `do-what-it-says`
