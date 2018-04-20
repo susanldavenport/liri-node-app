51f8b0eff1884997e6d1c0bfa7256f71ccef6359
SusanLDavenport <susanldavenport@gmail.com>
2018-04-20 18:17:05 -0400

    added a help function

--- liri.js
+++ liri.js
@@ -6,6 +6,7 @@ const Twitter = require('twitter');
 const Spotify = require('node-spotify-api');
 const request = require('request');
 const fs = require('fs');
+const help = require('inquirer'); 
 
 var liriComm = process.argv[2];
 var userInput = process.argv;
@@ -37,7 +38,6 @@ function runLiri(){
         break;
 
        case 'movie-this':
-            // console.log(userInput[0]);
             if (typeof userInput[0] === 'undefined') {
                 movieName = 'Sabrina';
                 omdbGrab();
@@ -50,15 +50,57 @@ function runLiri(){
         case 'do-what-it-says':
             doIt();
             break;
+        
+        case 'help':
+            getHelp();
+            break;
     }
 };
+
+function getHelp(){
+    console.log('in getHelp function');
+    help.prompt([
+        {
+            type: 'list',
+            message: 'What do you want to do?',
+            choices: ['my-tweets', 'spotify-this-song', 'movie-this', 'do-what-it-says'],
+            name: 'userHelp'
+        }
+    ])
+    .then(inputA => {
+        if (inputA.userHelp === 'spotify-this-song' || inputA.userHelp === 'movie-this') {
+            help.prompt([
+                {
+                type: 'input',
+                message: 'What would you like to search?',
+                name: 'userSearch'
+                }
+            ])
+            .then(inputB => {
+                liriComm = inputA.userHelp;
+                if (inputB.userSearch === ''){
+                    command.log('Make up your mind! Enter a search query!');
+                } else {
+                    liriComm = inputA.userHelp;
+                    userInput = inputB.userInput;
+                    console.log(userInput);
+                    userInput = userInput.split(' ');
+                    runLiri();
+                }
+            });
+        } else {
+            liriComm = inputA.userHelp;
+            runLiri();
+        }
+    });
+}
 
 function twitterGrab() {
     client.get('https://api.twitter.com/1.1/statuses/user_timeline.json', 
     function(err, tweets, response) {
         if (err) throw err;
         tweets.forEach(tweet => {
-            console.log('\n ================================');
+            console.log('\n ============!TWEET!=============');
             console.log(`Date: ${tweet.created_at}`);
             console.log(`${tweet.text}`);
             console.log('\n ================================');
@@ -71,12 +113,12 @@ function spotifyGrab() {
     spotify.search({type: "track", query: songName}, 
     function(err, data) {
         if(err) throw err;
-            console.log('=====================================');
+            console.log('=============*Spotify Results*=============');
             console.log(`Artist: ${data.tracks.items[0].artists[0].name}`);
             console.log(`Track: ${data.tracks.items[0].name}`); 
             console.log(`Album: ${data.tracks.items[0].album.name}`);
             console.log(`Link: ${data.tracks.items[0].album.external_urls.spotify}`);
-            console.log('=====================================');
+            console.log('===========================================');
         });
     };
 
@@ -86,7 +128,7 @@ function omdbGrab() {
     request(omdbURL, function(err, response, body){
         if (!err && response.statusCode === 200) {
             const data = JSON.parse(body);
-            console.log('=====================================');
+            console.log('===============*YOUR MOVIE*===============');
             console.log(`Title: ${data.Title}`);
             console.log(`Year: ${data.Year}`);
             console.log(`IMDB Rating: ${data.imdbRating}`);
@@ -95,7 +137,7 @@ function omdbGrab() {
             console.log(`Language: ${data.Language}`);
             console.log(`Actors: ${data.Actors}`);
             console.log(`Plot: ${data.Plot}`);
-            console.log('=====================================');
+            console.log('===========================================');
         };
     });
 };
